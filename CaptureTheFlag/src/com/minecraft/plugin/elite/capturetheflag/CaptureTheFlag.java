0adacfa19552ea45c9a56e3655b7033625389f41
BuildTools <unconfigured@null.spigotmc.org>
2017-07-01 00:20:42 +0200

    CTF Foundation

---
+++ CaptureTheFlag/src/com/minecraft/plugin/elite/capturetheflag/CaptureTheFlag.java
@@ -0,0 +1,10 @@
+package com.minecraft.plugin.elite.capturetheflag;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CaptureTheFlag extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+}
---
+++ CaptureTheFlag/src/com/minecraft/plugin/elite/capturetheflag/CaptureTheFlagLanguage.java
@@ -0,0 +1,6 @@
+package com.minecraft.plugin.elite.capturetheflag;
+
+import com.minecraft.plugin.elite.general.api.interfaces.LanguageNode;
+
+public enum CaptureTheFlagLanguage implements LanguageNode {
+}
