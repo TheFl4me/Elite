e17a5f9649e661186e778e77051b2601248b90c5
BuildTools <tristanfrehancock@gmail.com>
2017-07-10 16:15:54 +0200

    Bug Fixes

---
+++ KitPvP/src/com/minecraft/plugin/elite/kitpvp/commands/holograms/SetHologramCommand.java
@@ -0,0 +1,46 @@
+package com.minecraft.plugin.elite.kitpvp.commands.holograms;
+
+import com.minecraft.plugin.elite.general.api.abstracts.eCommand;
+import com.minecraft.plugin.elite.general.api.ePlayer;
+import com.minecraft.plugin.elite.general.database.Database;
+import com.minecraft.plugin.elite.kitpvp.KitPvP;
+import com.minecraft.plugin.elite.kitpvp.KitPvPLanguage;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SetHologramCommand extends eCommand {
+
+    public SetHologramCommand() {
+        super("sethologram", "ekitpvp.sethologram", false);
+    }
+
+    public boolean execute(CommandSender cs, Command cmd, String[] args) {
+        ePlayer p = ePlayer.get((Player) cs);
+        Database db = KitPvP.getDB();
+        Location loc = p.getPlayer().getLocation();
+        if(args.length > 0) {
+            String name = args[0].toLowerCase();
+            if(name.equalsIgnoreCase("feast") || name.equalsIgnoreCase("ehg")) {
+                if(db.containsValue(KitPvP.DB_HOLOGRAMS, "name", name)) {
+                    db.update(KitPvP.DB_HOLOGRAMS, "locx", loc.getX(), "name", name);
+                    db.update(KitPvP.DB_HOLOGRAMS, "locy", loc.getY(), "name", name);
+                    db.update(KitPvP.DB_HOLOGRAMS, "locz", loc.getZ(), "name", name);
+                } else {
+                    db.execute("INSERT INTO " + KitPvP.DB_HOLOGRAMS + " (name, locx, locy, locz) VALUES (?, ?, ?, ?);", name, loc.getX(), loc.getY(), loc.getZ());
+                }
+                p.getPlayer().sendMessage(p.getLanguage().get(KitPvPLanguage.HOLOGRAM_SET)
+                        .replaceAll("%holo", name));
+                KitPvP.reloadHolograms();
+                return true;
+            } else {
+                p.sendMessage(KitPvPLanguage.HOLOGRAM_USAGE);
+                return true;
+            }
+        } else {
+            p.sendMessage(KitPvPLanguage.HOLOGRAM_USAGE);
+            return true;
+        }
+    }
+}
