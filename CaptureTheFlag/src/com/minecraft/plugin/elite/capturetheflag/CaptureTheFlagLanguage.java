140b6b243646b434d1ab45de2c70d3d8d93663a9
BuildTools <tristanfrehancock@gmail.com>
2018-11-12 14:04:53 +0100

    Removed unused CTF Module

--- CaptureTheFlag/src/com/minecraft/plugin/elite/capturetheflag/CaptureTheFlagLanguage.java
+++
@@ -1,6 +0,0 @@
-package com.minecraft.plugin.elite.capturetheflag;
-
-import com.minecraft.plugin.elite.general.api.interfaces.LanguageNode;
-
-public enum CaptureTheFlagLanguage implements LanguageNode {
-}
